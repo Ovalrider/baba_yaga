3b4cc708f52f42f5ed6b0496c2c73fbb2e1255cb
Ovalrider <arten.shkoren@gmail.com>
2022-11-29 20:53:53 +0200

    third: fixed about.js img being in div

--- baba_yaga/src/pages/About.js
+++ baba_yaga/src/pages/About.js
@@ -7,8 +7,9 @@ function About(){
             <div className="big_yellow_text">
             <h1>Першокласнi послуги в токсичному i тривожному середовищi</h1>
             <h5>МIСIЯ НАШОГО САЛОНУ ЗРОБИТИ НАШИХ КЛIEНТIВ ЩАСЛИВИМИ, А ТАКОЖ ПIДКРЕСЛИТИ IНДИВIДУАЛЬНIСТЬ</h5>
-            <img src={babayaga} className="baba_yaga" />
             </div>
+            <img src={babayaga} className="baba_yaga" />
+            
         </div>
     )
 }
