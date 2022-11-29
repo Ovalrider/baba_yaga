c626be45c4eb98c31c64da4e20bfe91fe4e55bff
egor_slipchenko <40768233+yehorslipchenko@users.noreply.github.com>
2022-11-29 21:48:23 +0200

    Fix routing and remove unused files

--- baba_yaga/src/App.js
+++ baba_yaga/src/App.js
@@ -1,27 +1,13 @@
-import './App.css';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contacts from './pages/Contacts';
-import Review from './pages/Review';
-import Services from './pages/Services';
-import NavBar from './components/NavBar';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import {BrowserRouter} from "react-router-dom";
+import NavBar from "./components/NavBar";
+import AppRouter from "./components/AppRouter";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar/>
-      <Router>
-        <Routes>
-          <Route exact path='/' element={<Home/>}/>
-          <Route exact path='/services' element={<Services/>}/>
-          <Route exact path='/about' element={<About/>}/>
-          <Route exact path='/reviews' element={<Review/>}/>
-          <Route exact path='/contacts' element={<Contacts/>}/>
-        </Routes>
-      </Router>
-      
-    </div>
+      <BrowserRouter>
+          <NavBar />
+          <AppRouter />
+      </BrowserRouter>
   );
 }
 
