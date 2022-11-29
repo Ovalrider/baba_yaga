34e5562128929d56b0c4b2120c6769ced1e7e67c
unknown <arten.shkoren@gmail.com@>
2022-11-29 19:47:10 +0200

    first commit blank pages problem

---
+++ baba_yaga/src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Link} from "react-router-dom"
+//доробити іконки
+function NavBar(){
+    return(
+        <ul>
+            <li><Link to="/">Спа салон Баби Яги</Link></li> 
+            <li><Link to="/about">Про салон</Link></li>
+            <li><Link to="/services">Наші послуги</Link></li>
+            <li><Link to="/reviews">Відгуки</Link></li>
+            <li><Link to="/contacts">Контакти</Link></li>
+        </ul>
+    )
+}
+export default NavBar;
---
+++ baba_yaga/src/pages/Contacts.js
@@ -0,0 +1,14 @@
+import React from "react";
+import babayaga from "../assets/pics/home_left_baba.jpg"
+
+function Contacts(){
+    return(
+        <div className="contacts_body">
+            <div className="big_yellow_text">
+            <h1>ASad</h1>
+            <img src={babayaga} className="baba_yaga" />
+            </div>
+        </div>
+    )
+}
+export default Contacts;
