4c1e8b0481d6b9f8ab97820e59c162bd9cafbdf4
Ovalrider <arten.shkoren@gmail.com>
2022-12-01 17:33:34 +0200

    about page + fixed flex navbar

--- baba_yaga/src/components/NavBar/NavBar.jsx
+++ baba_yaga/src/components/NavBar/NavBar.jsx
@@ -1,29 +1,31 @@
 import React from "react";
 import {Link} from "react-router-dom"
 import "../NavBar/NavBar.css"
+import logosm from "../../assets/pics/menu-item-icon.png"
+import logomain from "../../assets/pics/nav-logo.png"
 
 function NavBar(){
     return(
         <div className="header-menu_container">
-            <div className="logo-block">
-                <div className="logo-image"></div>
-                <Link className="menu-item_link" to="/">Спа салон Баби Яги</Link>
-            </div>
             <ul className="header-menu">
                 <li className="menu-item">
-                    <div className="item-icon-1"></div>
+                <div ><img src={logomain} alt="logo" className="logo-image"/></div>
+                <Link className="menu-item_link" to="/">Спа салон Баби Яги</Link>
+                </li>
+                <li className="menu-item">
+                    <div > <img src={logosm} alt="logo" className="item-icon"/></div>
                     <Link className="menu-item_link" to="/about">Про салон</Link>
                 </li>
                 <li className="menu-item">
-                    <div className="item-icon-2"></div>
+                    <div ><img src={logosm} alt="logo" className="item-icon"/></div>
                     <Link className="menu-item_link" to="/services">Наші послуги</Link>
                 </li>
                 <li className="menu-item">
-                    <div className="item-icon-3"></div>
+                    <div ><img src={logosm} alt="logo" className="item-icon"/></div>
                     <Link className="menu-item_link" to="/reviews">Відгуки</Link>
                 </li>
                 <li className="menu-item">
-                    <div className="item-icon-4"></div>
+                    <div ><img src={logosm} alt="logo" className="item-icon"/></div>
                     <Link className="menu-item_link" to="/contacts">Контакти</Link>
                 </li>
             </ul>
